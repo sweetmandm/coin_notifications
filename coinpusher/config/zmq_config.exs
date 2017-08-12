844dfa65e78a93898fe8bdf2c2993410b2d474b4
David Sweetman <david@davidsweetman.com>
2017-08-12 02:13:58 +0000

    Provide ZMQ config to the elixir app

---
+++ coinpusher/config/zmq_config.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :coinpusher,
+  zmq_address: "127.0.0.1",
+  zmq_port: 28332
--- coinpusher/lib/coinpusher.ex
+++ coinpusher/lib/coinpusher.ex
@@ -1,24 +1,27 @@
 defmodule CoinPusher.ZMQClient do
   def main do
-    IO.puts "starting"
     {:ok, socket} = :chumak.socket(:sub)
+    subscribe(socket)
+    {:ok, _pid} = :chumak.connect(socket, :tcp, zmq_address(), zmq_port())
+    loop(socket)
+  end
+
+  defp zmq_address do
+    to_charlist(Application.get_env(:coinpusher, :zmq_address))
+  end
+
+  defp zmq_port do
+    Application.get_env(:coinpusher, :zmq_port)
+  end
+
+  defp subscribe(socket) do
     :chumak.subscribe(socket, 'hashblock')
     :chumak.subscribe(socket, 'hashtx')
     :chumak.subscribe(socket, 'rawblock')
     :chumak.subscribe(socket, 'rawtx')
-    case :chumak.bind(socket, :tcp, '127.0.0.1', 5555) do
-      {:ok, _bind_pid} ->
-        IO.puts "connected socket"
-      {:error, reason} ->
-        IO.puts "connection failed"
-        IO.inspect reason
-      other ->
-        IO.inspect other
-    end
-    loop(socket)
   end
 
-  def loop(socket) do
+  defp loop(socket) do
     IO.puts "waiting"
     {:ok, data} = :chumak.recv_multipart(socket)
     IO.inspect data
