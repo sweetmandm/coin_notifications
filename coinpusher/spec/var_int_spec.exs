44fc621504bf3ebd3c3ecf41980e22bf3655dc60
David Sweetman <david@davidsweetman.com>
2017-08-13 03:28:47 +0000

    Add a spec for var_int

---
+++ coinpusher/spec/var_int_spec.exs
@@ -0,0 +1,30 @@
+defmodule VarIntSpec do
+  use ESpec
+
+  subject do: CoinPusher.VarInt.parse(data)
+
+  describe "uint8" do
+    let :data, do: <<1>>
+    it do: should eq({:ok, 1, <<>>})
+  end
+
+  describe "uint16" do
+    let :data, do: <<0xFD, 0x00, 0xFF>>
+    it do: should eq({:ok, 65280, <<>>})
+  end
+
+  describe "uint32" do
+    let :data, do: <<0xFE, 0x00, 0x00, 0x00, 0xFF>>
+    it do: should eq({:ok, 4278190080, <<>>})
+  end
+
+  describe "uint64" do
+    let :data, do: <<0xFF, 0x00 ,0x00, 0x00 ,0x00, 0x00, 0x00, 0x00, 0xFF>>
+    it do: should eq({:ok, 18374686479671623680, <<>>})
+  end
+
+  describe "extra data" do
+    let :data, do: <<0xFD, 0x00, 0xFF, "Hello">>
+    it do: should eq({:ok, 65280, <<"Hello">>})
+  end
+end
