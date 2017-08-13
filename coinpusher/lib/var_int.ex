85c6cf2c7f8b7ae0eb778f9b109791afe2e7eb29
David Sweetman <david@davidsweetman.com>
2017-08-13 01:34:31 +0000

    Simplify VarInt methods

--- coinpusher/lib/var_int.ex
+++ coinpusher/lib/var_int.ex
@@ -1,22 +1,20 @@
 # https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
 defmodule CoinPusher.VarInt do
-  def parse(data), do: parse_int(data)
-
-  defp parse_int(<<0xFF, value :: unsigned-integer-little-64, rest :: binary>>) do
+  def parse(<<0xFF, value :: unsigned-integer-little-64, rest :: binary>>) do
     {:ok, value, rest}
   end
 
-  defp parse_int(<<0xFE, value :: unsigned-integer-little-32, rest :: binary>>) do
+  def parse(<<0xFE, value :: unsigned-integer-little-32, rest :: binary>>) do
     {:ok, value, rest}
   end
 
-  defp parse_int(<<0xFD, value :: unsigned-integer-little-16, rest :: binary>>) do
+  def parse(<<0xFD, value :: unsigned-integer-little-16, rest :: binary>>) do
     {:ok, value, rest}
   end
 
-  defp parse_int(<<value :: unsigned-integer-8, rest :: binary>>) do
+  def parse(<<value :: unsigned-integer-8, rest :: binary>>) do
     {:ok, value, rest}
   end
 
-  defp parse_int(_other), do: :error
+  def parse(_other), do: :error
 end
