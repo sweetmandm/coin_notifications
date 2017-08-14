8532c610c5beaef3ee28b9be800e4d628d6480ae
David Sweetman <david@davidsweetman.com>
2017-08-14 03:12:45 +0000

    Parse the transaction outputs and lock time

--- coinpusher/lib/primitives/raw_transaction.ex
+++ coinpusher/lib/primitives/raw_transaction.ex
@@ -1,8 +1,8 @@
 # https://en.bitcoin.it/wiki/Protocol_documentation#tx
 defmodule CoinPusher.RawTransaction do
-  alias CoinPusher.{VarInt, TxIn}
+  alias CoinPusher.{VarInt, TxIn, TxOut}
 
-  defstruct [:version, :tx_in]
+  defstruct [:version, :tx_in, :tx_out, :lock_time]
 
   def parse(data) do
     <<version :: signed-integer-little-32, rest :: binary >> = data
@@ -12,20 +12,27 @@ defmodule CoinPusher.RawTransaction do
 
   defp parse(version = 2, data) do
     {:ok, tx_in_count, data} = VarInt.parse(data)
-    {:ok, tx_in_list, _data} = parse_tx_in_list([], 0, tx_in_count, data)
+    {:ok, tx_in_list, data} = parse_list(tx_in_count, data, &TxIn.parse/1)
+    {:ok, tx_out_count, data} = VarInt.parse(data)
+    {:ok, tx_out_list, data} = parse_list(tx_out_count, data, &TxOut.parse/1)
+    <<lock_time :: unsigned-integer-32>> = data
     %CoinPusher.RawTransaction{
       version: version,
-      tx_in: tx_in_list
+      tx_in: tx_in_list,
+      tx_out: tx_out_list,
+      lock_time: lock_time
     }
   end
 
-  defp parse_tx_in_list(tx_in_list, max, max, data) do
-    {:ok, tx_in_list, data}
+  defp parse_list(list \\ [], index \\ 0, count, data, func)
+
+  defp parse_list(list, max, max, data, _func) do
+    {:ok, list, data}
   end
 
-  defp parse_tx_in_list(tx_in_list, index, count, data) do
-    {:ok, tx_in, data} = TxIn.parse(data)
-    parse_tx_in_list(tx_in_list ++ [tx_in], index + 1, count, data)
+  defp parse_list(list, index, count, data, func) do
+    {:ok, item, data} = func.(data)
+    parse_list(list ++ [item], index + 1, count, data, func)
   end
 
   def is_coinbase?(raw_transaction) do
---
+++ coinpusher/lib/primitives/tx_out.ex
@@ -0,0 +1,17 @@
+# https://en.bitcoin.it/wiki/Protocol_documentation#tx
+defmodule CoinPusher.TxOut do
+  alias CoinPusher.VarInt
+
+  defstruct [:value, :pk_script]
+
+  def parse(data) do
+    <<value :: integer-little-64, data :: binary>> = data
+    {:ok, pk_script_length, data} = VarInt.parse(data)
+    <<pk_script :: binary-size(pk_script_length), data :: binary>> = data
+    tx_out = %CoinPusher.TxOut{
+      value: value,
+      pk_script: pk_script
+    }
+    {:ok, tx_out, data}
+  end
+end
