def908bbe53a0855cbc997409a8d617542ba8223
David Sweetman <david@davidsweetman.com>
2017-08-26 03:46:41 +0000

    minor updates to the contact docs

--- coinpusher/lib/notifications/contact.ex
+++ coinpusher/lib/notifications/contact.ex
@@ -2,10 +2,9 @@ defmodule CoinPusher.Contact do
   require Logger
   alias CoinPusher.Twilio
 
-  @type phone_number :: String.t
-
   defstruct [:phone_number]
 
+  @spec notify(%__MODULE__{}, String.t) :: any
   def notify(contact, message) do
     Logger.debug "Notification | #{contact.phone_number}: #{message}"
     Twilio.sms(contact.phone_number, message)
