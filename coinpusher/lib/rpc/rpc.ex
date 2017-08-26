87c4ffb533dbbedf60f1e700ae1e190abb3742d6
David Sweetman <david@davidsweetman.com>
2017-08-26 03:40:41 +0000

    Pull rpc url from application config

--- coinpusher/lib/rpc/rpc.ex
+++ coinpusher/lib/rpc/rpc.ex
@@ -5,24 +5,24 @@ defmodule CoinPusher.RPC do
 
   @spec get_raw_transaction(String.t) :: rpc_result
   def get_raw_transaction(hash) do
-    HTTPClient.call("http://127.0.0.1:18332/", "getrawtransaction", [hash], [auth_header()])
+    HTTPClient.call(url(), "getrawtransaction", [hash], [auth_header()])
   end
 
   @spec get_info :: rpc_result
   def get_info do
-    HTTPClient.call("http://127.0.0.1:18332/", "getinfo", [], [auth_header()])
+    HTTPClient.call(url(), "getinfo", [], [auth_header()])
   end
 
   @spec get_best_block_hash :: rpc_result
   def get_best_block_hash do
-    HTTPClient.call("http://127.0.0.1:18332/", "getbestblockhash", [], [auth_header()])
+    HTTPClient.call(url(), "getbestblockhash", [], [auth_header()])
   end
 
   defp auth_header do
-    {"Authorization", "Basic #{rpc_pair()}"}
+    {"Authorization", "Basic #{rpc_auth_pair()}"}
   end
 
-  defp rpc_pair do
+  defp rpc_auth_pair do
     "#{rpc_user()}:#{rpc_password()}" |> Base.encode64
   end
 
@@ -32,5 +32,17 @@ defmodule CoinPusher.RPC do
 
   defp rpc_password do
     Application.get_env(:coinpusher, :rpc_pass)
+  end
+
+  defp url do
+    "#{rpc_address()}:#{rpc_port()}"
+  end
+
+  defp rpc_address do
+    Application.get_env(:coinpusher, :rpc_address)
+  end
+
+  defp rpc_port do
+    Application.get_env(:coinpusher, :rpc_port)
   end
 end
